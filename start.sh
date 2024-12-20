f840fd2d31d6973a5ef68a4c59b3b350c3c8bf33
wangxiaofengx <605280610@qq.com>
2024-12-20 21:48:40 +0800

    添加字符集
    添加字符集

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 # 设置终端标题
 echo -ne "\033]0;video-conference\007"
 
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 # 执行 Java 程序
-java -Xms128m -Xmx128m -jar ./target/video-conference.jar
+java -Dfile.encoding=UTF-8 -Xms128m -Xmx128m -jar "$SCRIPT_DIR/target/video-conference.jar"
