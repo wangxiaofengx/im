c6a29c8888e5e5a224f48f75891565d2451fe622
wangxiaofengx <605280610@qq.com>
2024-12-24 21:38:50 +0800

    添加ice地址

--- src/main/java/com/conference/service/IceService.java
+++ src/main/java/com/conference/service/IceService.java
@@ -1,19 +1,26 @@
 package com.conference.service;
 
 import lombok.SneakyThrows;
-import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.File;
-import java.net.URI;
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class IceService {
+
+    private static final Logger log = LoggerFactory.getLogger(IceService.class);
 
     @Value("${ice.stun-server-url}")
     String iceServerUrl;
@@ -26,14 +33,40 @@ public class IceService {
         this.restTemplate = restTemplate;
     }
 
+    @PostConstruct
+    public void init() {
+        try {
+            this.fetchIceServers();
+        } catch (Exception e) {
+            log.error("Failed to fetch ice server urls", e);
+            this.readIceFile();
+        }
+        log.info("ice server list: {}",iceServers);
+    }
 
     @Scheduled(cron = "0 0 3 * * *")
+    public void trackIceServer() {
+        try {
+            this.fetchIceServers();
+        } catch (Exception e) {
+            log.error("Failed to fetch ice server urls", e);
+        }
+    }
+
     @SneakyThrows
-    public List<String> getIceUrl() {
+    public void fetchIceServers() {
+        log.info("Fetching ice server urls from {}", iceServerUrl);
+        String response = restTemplate.getForObject(iceServerUrl, String.class);
+        this.iceServers = Arrays.asList(response.split("\\r?\\n"));
+        log.info("ice server urls {}", iceServers);
+    }
 
-        this.iceServers = FileUtils.readLines(new File(new URI(iceServerUrl)), StandardCharsets.UTF_8);
-
-        return this.iceServers;
+    @SneakyThrows
+    public void readIceFile() {
+        ClassPathResource resource = new ClassPathResource("data/valid_hosts.txt");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8))) {
+            this.iceServers = reader.lines().collect(Collectors.toList());
+        }
     }
 
     public List<String> getIceServers() {
