5bff9e8517261df1ec61d8c003fffeab4cbf8651
wangxiaofengx <605280610@qq.com>
2026-07-16 16:31:01 +0800

    打包和启动方式修改为jdk21

--- start.bat
+++ start.bat
@@ -1,3 +1,19 @@
 @echo off
+setlocal
 title video-conference
-java -Dfile.encoding=UTF-8 -jar -Xms128m -Xmx128m target\video-conference.jar
+
+if not defined JAVA_21_HOME (
+    echo ERROR: JAVA_21_HOME is not set.
+    exit /b 1
+)
+
+if not exist "%JAVA_21_HOME%\bin\java.exe" (
+    echo ERROR: Java was not found under JAVA_21_HOME: "%JAVA_21_HOME%"
+    exit /b 1
+)
+
+set "JAVA_HOME=%JAVA_21_HOME%"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
+
+"%JAVA_HOME%\bin\java.exe" -Dfile.encoding=UTF-8 -Xms128m -Xmx128m -jar target\video-conference.jar
+exit /b %ERRORLEVEL%
--- z-package.bat
+++ z-package.bat
@@ -1,2 +1,18 @@
 @echo off
-mvn clean package
+setlocal
+
+if not defined JAVA_21_HOME (
+    echo ERROR: JAVA_21_HOME is not set.
+    exit /b 1
+)
+
+if not exist "%JAVA_21_HOME%\bin\java.exe" (
+    echo ERROR: Java was not found under JAVA_21_HOME: "%JAVA_21_HOME%"
+    exit /b 1
+)
+
+set "JAVA_HOME=%JAVA_21_HOME%"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
+
+call mvn clean package
+exit /b %ERRORLEVEL%
